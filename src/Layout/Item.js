04303774a49b9f8aed00fe0fc0a52afab452e067
KaustavG1 <mail2kg2@gmail.com>
2021-01-26 01:03:00 +0530

    Minor Changes

--- src/Layout/Item.js
+++ src/Layout/Item.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react';
 import './Item.css';
 
 class Item extends Component {
+    /* constructor(props) {
+        super(props);
+        this.handleEdit = this.handleEdit.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
+    } */
+
+    handleEdit(event) {
+        console.log("Edited");
+    }
+
+    handleDelete(event) {
+        console.log("Deleted");
+    }
+
     render() {
         return (<div className="Item">
             <span className="item-text">{this.props.textVal}</span>
             <div className="icons">
-                <i className="edit fas fa-pen"></i>
-                <i className="delete fas fa-trash"></i>
+                <i className="edit fas fa-pen" onClick={this.handleEdit}></i>
+                <i className="delete fas fa-trash" onClick={this.handleDelete}></i>
             </div>
         </div>);
     }
